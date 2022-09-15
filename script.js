48e01f71b0b386863a668e9bda5d34e22a2c07ff
Ayush Jain <jnayush15@gmail.com>
2022-09-15 09:58:22 +0530

    Handling of check btn press is done.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!!';
+document.querySelector('.number').textContent = 15;
+document.querySelector('.score').textContent = 25;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!!';
+  }
+});

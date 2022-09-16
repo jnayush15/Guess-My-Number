52a6c938ccf644c37986f6f70e2e2f76f02afa87
Ayush Jain <jnayush15@gmail.com>
2022-09-16 12:14:29 +0530

    Removed some unecessary comments

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!!';
-document.querySelector('.number').textContent = 15;
-document.querySelector('.score').textContent = 25;
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
-
 let secretNumber = 0;
 let score = 20;
 let highScore = 0;
@@ -53,7 +44,6 @@ document.querySelector('.check').addEventListener('click', function () {
     displayNumber(secretNumber);
     manipulateCSS('#60b347', '30rem');
     highScore = highScore > score ? highScore : score;
-    // document.querySelector('.highscore').textContent = highScore;
     displayHighScore(highScore);
   } else if (guess !== secretNumber) {
     if (score > 1) {
